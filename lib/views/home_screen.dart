fc748e367390b72a2ecec4603c06dbddfae2bd0c
mahartanveerahmad <mahartanveerahmad@gmail.com>
2023-02-06 19:31:17 +0500

    tabs created

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: const Text("WhatsApp"),
+          actions: const [
+            IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.camera_alt_outlined,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.search_outlined,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ),
+            ),
+          ],
+          bottom: const TabBar(
+            indicatorColor: Colors.white,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.group_outlined),
+              ),
+              Tab(
+                text: "Chats",
+              ),
+              Tab(
+                text: "status",
+              ),
+              Tab(
+                text: "Calls",
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Text("Community"),
+            Text("Chats"),
+            Text("status"),
+            Text("calls"),
+          ],
+        ),
+      ),
+    );
   }
 }
